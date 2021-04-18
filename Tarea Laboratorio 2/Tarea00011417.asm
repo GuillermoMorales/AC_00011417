71889ab4a136190afdf9837af3fb893b6595a634
GuillermoMorales <37193751+GuillermoMorales@users.noreply.github.com>
2021-04-17 23:58:16 -0600

    Update Tarea00011417.asm

--- Tarea Laboratorio 2/Tarea00011417.asm
+++ Tarea Laboratorio 2/Tarea00011417.asm
@@ -1,21 +1,21 @@
-    org 	100h
+    	org 	100h
 
-	  section	.text
+	section	.text
 
-	  mov byte [200h], "G"
-    mov byte [201h], "J"
-    mov byte [202h], "M"
-    mov byte [203h], "C"
-    mov	BX, 200h
-    mov SI, 2d
+        mov byte [200h], "G"
+    	mov byte [201h], "J"
+    	mov byte [202h], "M"
+    	mov byte [203h], "C"
+    	mov	BX, 200h
+    	mov SI, 2d
 
-;   Direccionamiento directo o absoluto
-    mov AL, [200h]
-;   Direccionamiento indirecto por registro
-    mov	CL, [BX]
-;   Direccionamiento indirecto por base+índice
-    mov 	DL, [BX+SI]
-;   Direccionamiento relativo por registro
-	  mov     DI, [BX+3h]
+;   	Direccionamiento directo o absoluto
+    	mov AL, [200h]
+;   	Direccionamiento indirecto por registro
+    	mov	CL, [BX]
+;   	Direccionamiento indirecto por base+índice
+    	mov 	DL, [BX+SI]
+;   	Direccionamiento relativo por registro
+	mov     DI, [BX+3h]
     
-	  int	20h
+	int	20h
