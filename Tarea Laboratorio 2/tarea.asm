f3953b44d6a902bdce5794811036d86f457a29ff
GuillermoMorales <37193751+GuillermoMorales@users.noreply.github.com>
2021-04-17 23:59:13 -0600

    Add files via upload

---
+++ Tarea Laboratorio 2/tarea.asm
@@ -0,0 +1,21 @@
+    org 	100h
+
+	section	.text
+
+	mov byte [200h], "G"
+    mov byte [201h], "J"
+    mov byte [202h], "M"
+    mov byte [203h], "C"
+    mov	BX, 200h
+    mov SI, 2d
+
+;   Direccionamiento directo o absoluto
+    mov AL, [200h]
+;   Direccionamiento indirecto por registro
+    mov	CL, [BX]
+;   Direccionamiento indirecto por base+índice
+    mov 	DL, [BX+SI]
+; Direccionamiento relativo por registro
+	mov     DI, [BX+3h]
+    
+	int	20h
